435430110f856ff94d751a692c9ae825bb84358c
Kirill Andreev <kirill.andreev@klarna.com>
2025-12-01 12:46:35 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,40 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var zeroes = 0
+        var cur = 50
+        input.forEach { r ->
+            val direction = if (r[0] == 'R') 1 else -1
+            val count = r.substring(1).toInt() * direction
+            cur = Math.floorMod(cur + count, 100)
+            if (cur == 0) zeroes++
+        }
+        return zeroes
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var zeroes = 0
+        var cur = 50
+        input.forEach { r ->
+            val start0 = cur == 0
+
+            val direction = if (r[0] == 'R') 1 else -1
+            val count = r.substring(1).toInt() * direction
+
+            cur += count
+            if (cur < 0) {
+                zeroes += (-cur / 100)
+                if (!start0) zeroes++ // when 0 -> -5 we didnt cross 0
+            } else if (cur > 0) {
+                zeroes += (cur / 100)
+            } else {
+                zeroes++
+            }
+
+            cur = Math.floorMod(cur, 100)
+        }
+        return zeroes
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
